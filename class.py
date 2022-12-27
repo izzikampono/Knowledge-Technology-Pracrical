185c33fe8494dc62bc6b19f02f258a656b83e101
izzikampono <58926615+izzikampono@users.noreply.github.com>
2022-12-27 13:17:10 +0100

    elimation function implemented

--- class.py
+++ class.py
@@ -9,22 +9,59 @@ def loadKB(file):
     data = json.load(f)
   
     # Iterating through the json list
-    for i in data:
-        print(i)
+    # for i in data:
+    #     print(i)
   
     # Closing file
     f.close()
     return data
 
 class Compound():
+    name=""
     nature=""
-    num_atoms=0
+    num_atoms=""
     position=""
     nature_hydro= ""
-    Reactivity=""
-    State =""
-    Polarity=""
-    fact_base=loadKB("kb.json")
+    reactivity=""
+    state =""
+    polarity=""
+    fact_base=loadKB("kb2.json")
+    possible_list=[]
+
+    @classmethod
+    def initialize_possibilities(cls):
+        for i in cls.fact_base:
+            cls.possible_list.append(str(i["name"]))
+        #print(f"INITIALIZED : {cls.possible_list}\n")
+    
+    def eliminate(self):
+        """" this function checks the existing attributes of the compound 
+        and eliminates all other compounds that do not have the same attributes"""
+
+        self.initialize_possibilities()
+        this_compound = self.__dict__
+        for compound in self.fact_base:
+            for i,j in this_compound.items():
+                #print(i,j)
+                if i=="name":
+                    pass
+                    print("YAY")
+                if this_compound[i]!=compound[i]:
+                    try: 
+                        self.possible_list.remove(compound["name"])
+                        print("removed {}".format(compound["name"]))
+                        break
+                    except:
+                        print("{} not in list".format(compound["name"]))
+            
+        print(f"list of possible compounds : {self.possible_list}")
+        return
+
+
+
+
+
+
 
 
     def init(self,nature,num_atoms,position,nature_hydro,reactivity,state,polarity):
@@ -37,25 +74,50 @@ class Compound():
         self.polarity=polarity
         return
 
-    def export(self):
-        dictionary={"nature" : self.nature,
+    @classmethod
+    def to_dict(self):
+        dictionary={
+            "name" : self.name,
+            "nature" : self.nature,
             "num_atoms" : self.num_atoms,
             "position" : self.position,
             "nature_hydro" : self.nature_hydro,
-            "Reactivity" : self.Reactivity,
-            "State" : self.State,
-            "Polarity" : self.polarity,
+            "reactivity" : self.reactivity,
+            "state" : self.state,
+            "polarity" : self.polarity,
             "num_atoms" : self.num_atoms }
         return dictionary
 
 def Match(Compound,kb_name):
+    "IN MATCH"
     KB=loadKB(kb_name)
-    counter = 0
-    for i,j in zip(Compound,KB):
-        if i==j:
-            counter+=1
+    Compound=Compound.__dict__
+    counter = {}
+    for Halogen in KB:
+        counter[Halogen["name"]]=0
+        for i,j in zip(Halogen,Compound):
+            if j=="name":
+                pass
+            if Halogen[i]==Compound[j]:
+                print("The compound has {} of {} which is the same as  {} that has {} ".format(i,Halogen[i],Compound["name"],Compound[j]))
+                counter[Halogen["name"]]+=1
+        print("\n NEW")
+    print(max(counter))
+    
     
     return counter
+
+
+
+test = Compound()
+setattr(test,'nature',"Chlorinated")
+setattr(test,"num_atoms","Monohalogenated")
+#setattr(test,"position","Saturated")
+print(test.nature)
+test.eliminate()
+
+
+#Match(test,"kb2.json")
 
 
     
