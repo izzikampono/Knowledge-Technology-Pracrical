4236dc4116a95901de910df964e1435f3233d690
izzikampono <58926615+izzikampono@users.noreply.github.com>
2022-12-30 16:07:04 +0100

    functions to manipulate xml file

--- test.py
+++ test.py
@@ -1,50 +1,80 @@
 import xml.dom.minidom
 import xml.etree.ElementTree as ET
 
-domTree = xml.dom.minidom.parse("rules.xml")
 Tree = ET.parse("rules.xml")
 root =Tree.getroot()
 
 tag=root.tag
 print(tag)
 
-group = domTree.documentElement
+for child in root:
+    print(child.tag, child.attrib)
+print("===================")
 
-facts = group.getElementsByTagName('fact')
-questions = group.getElementsByTagName('question')
-# facts_in_question=questions.getElementByTagName('fact')
 
-for c in root.findall('question'):
-    att = c.attrib
-    print(att)
-    desc = c.findall("description")
-    for i in desc:
-        print(i.tag,i.attrib,i.text)
+
+def newFact(Tree,name,value):
+    root =Tree.getroot()
+    factBase=root.find('factBase')
+    facts = factBase.findall('fact')
+
+    newElement = ET.SubElement(factBase,"fact")
+    newElement.set("name",name)
+    newElement.text=value
+    Tree.write("rules2.xml")
+
+    # for fact in facts:
+    #     print(fact.tag,fact.attrib,fact.text)
+
+def checkFactBase(Tree,name,value):
+    #checks if a particular fact is in the factBase
+    root =Tree.getroot()
+    factBase=root.find('factBase')
+    facts = factBase.findall('fact')
+
+    for fact in facts:
+        if fact.attrib==name and fact.text==value:
+            return True
+
+
+def updateFacts(Tree):
+    root =Tree.getroot()
+    rules = root.findall("rule")
+    print(rules)
+    counter=0
+    for rule in rules:
+        print(counter)
+        ifs = rule.find(".//fact")
+        print(ifs.tag,ifs.attrib,ifs.text)
+        counter+=1
+
+
 
 
 def getQuestion(nodelist):
-    desc=nodelist.findall("description")
+    #finds all child/grandchild nodes with tag == 'description'
+    desc=nodelist.findall(".//description")
 
-    question = desc[0]
-    print(question.text)
-    options = desc[1:]
-    for j in options:
+    question = desc
+    options = desc[:]
+    for j in desc:
         print(j.text)
     return(desc)
 
 
+
+
+
+# ##############################################################################################################################
 for i in root.findall('question'):
     getQuestion(i)
 
 
+#newFact(Tree,"Saturated","True")
+updateFacts(Tree)
+checkFactBase(Tree)
 
-# for i in facts:
-#     print(f"{i.getAttribute('name')} --> {i.childNodes[0].nodeValue}" )
 
-# for i in questions:
-#     print(f"{i.find('description').text}")
-
-#print(facts_in_question.childNodes[0].nodeValue)
 
 
 
