caf2e6a20ff62ae2c2b1c83d1d7c1a3e480b2cb2
cristina-ropot <c.a.ropot@student.rug.nl>
2023-01-13 14:47:11 +0100

    newwww

--- forGUI.py
+++ forGUI.py
@@ -1,27 +1,100 @@
-import dearpygui.dearpygui as dpg
 
-dpg.create_context()
-
-def add_buttons():
-    global new_button1, new_button2
-    new_button1 = dpg.add_button(label="New Button", before="delete_button", tag="new_button1")
-    new_button2 = dpg.add_button(label="New Button 2", parent="secondary_window", tag="new_button2")
-
-
-def delete_buttons():
-    dpg.delete_item("new_button1")
-    dpg.delete_item("new_button2")
-
-
-with dpg.window(label="Tutorial", pos=(200, 200)):
-    dpg.add_button(label="Add Buttons", callback=add_buttons)
-    dpg.add_button(label="Delete Buttons", callback=delete_buttons, tag="delete_button")
-
-with dpg.window(label="Secondary Window", tag="secondary_window", pos=(100, 100)):
-    pass
-
-dpg.create_viewport(title='Custom Title', width=600, height=400)
-dpg.setup_dearpygui()
-dpg.show_viewport()
-dpg.start_dearpygui()
-dpg.destroy_context()
+# def frame_button2():
+#     clear_widgets()
+#     # question widget
+#     question = QLabel("QUESTION2button")
+#     question.setAlignment(QtCore.Qt.AlignCenter)
+#     question.setWordWrap(True)
+#     question.setStyleSheet(
+#         "font-family: Shanti;" +
+#         "font-size: 25px;" +
+#         "color: 'white';" +
+#         "padding: 75px;"
+#
+#     )
+#     widgets["question"].append(question)
+#     create_buttons()
+#     button1 = create_buttons("answer1", 85, 5)
+#     button2 = create_buttons("answer2", 5, 85)
+#
+#     widgets["answer1"].append(button1)
+#     widgets["answer2"].append(button2)
+#
+#     # place widget on the grid
+#     grid.addWidget(widgets["question"][-1], 1, 0, 1, 2)
+#     grid.addWidget(widgets["answer1"][-1], 2, 0)
+#     grid.addWidget(widgets["answer2"][-1], 2, 1)
+#
+# def frame_button4():
+#     clear_widgets()
+#     # question widget
+#     question = QLabel("QUESTION4button")
+#     question.setAlignment(QtCore.Qt.AlignCenter)
+#     question.setWordWrap(True)
+#     question.setStyleSheet(
+#         "font-family: Shanti;" +
+#         "font-size: 25px;" +
+#         "color: 'white';" +
+#         "padding: 75px;"
+#
+#     )
+#     widgets["question"].append(question)
+#     create_buttons()
+#     # answer button widgets
+#     button1 = create_buttons("answer1", 85, 5)
+#     button2 = create_buttons("answer2", 5, 85)
+#     button3 = create_buttons("answer3", 85, 5)
+#     button4 = create_buttons("answer4", 5, 85)
+#
+#     widgets["answer1"].append(button1)
+#     widgets["answer2"].append(button2)
+#     widgets["answer3"].append(button3)
+#     widgets["answer4"].append(button4)
+#
+#     # place widget on the grid
+#     grid.addWidget(widgets["question"][-1], 1, 0, 1, 2)
+#     grid.addWidget(widgets["answer1"][-1], 2, 0)
+#     grid.addWidget(widgets["answer2"][-1], 2, 1)
+#     grid.addWidget(widgets["answer3"][-1], 3, 0)
+#     grid.addWidget(widgets["answer4"][-1], 3, 1)
+#
+# def frame_input_text():
+#     clear_widgets()
+#     # question widget
+#     question = QLabel("QUESTION4button")
+#     question.setAlignment(QtCore.Qt.AlignCenter)
+#     question.setWordWrap(True)
+#     question.setStyleSheet(
+#         "font-family: Shanti;" +
+#         "font-size: 25px;" +
+#         "color: 'white';" +
+#         "padding: 75px;"
+#
+#     )
+#     widgets["question"].append(question)
+#
+#     textbox = QLineEdit()
+#     textbox.setStyleSheet(
+#         '''
+#         *{
+#             border: 4px solid '#BC006C';
+#             border-radius: 45px;
+#             font-size: 35px;
+#             color: 'white';
+#             padding: 25px 0;
+#             margin: 100px 200px;
+#         }
+#         *:hover{
+#             background: '#BC006C';
+#         }
+#         '''
+#     )
+#     textbox.move(20, 20)
+#     textbox.resize(280, 40)
+#
+# def next_question_frame():
+#     # display frame for next question
+#     clear_widgets()
+#
+#     #dict = utils.makeDictionaryBool(model.current_question)
+#
--- guiTry.py
+++ guiTry.py
@@ -52,6 +52,108 @@ def start():
     frame2()
 
 
+
+def frame_button2():
+    clear_widgets()
+    # question widget
+    question = QLabel("QUESTION2button")
+    question.setAlignment(QtCore.Qt.AlignCenter)
+    question.setWordWrap(True)
+    question.setStyleSheet(
+        "font-family: Shanti;" +
+        "font-size: 25px;" +
+        "color: 'white';" +
+        "padding: 75px;"
+
+    )
+    widgets["question"].append(question)
+    create_buttons()
+    button1 = create_buttons("answer1", 85, 5)
+    button2 = create_buttons("answer2", 5, 85)
+
+    widgets["answer1"].append(button1)
+    widgets["answer2"].append(button2)
+
+    # place widget on the grid
+    grid.addWidget(widgets["question"][-1], 1, 0, 1, 2)
+    grid.addWidget(widgets["answer1"][-1], 2, 0)
+    grid.addWidget(widgets["answer2"][-1], 2, 1)
+
+def frame_button4():
+    clear_widgets()
+    # question widget
+    question = QLabel("QUESTION4button")
+    question.setAlignment(QtCore.Qt.AlignCenter)
+    question.setWordWrap(True)
+    question.setStyleSheet(
+        "font-family: Shanti;" +
+        "font-size: 25px;" +
+        "color: 'white';" +
+        "padding: 75px;"
+
+    )
+    widgets["question"].append(question)
+    create_buttons()
+    # answer button widgets
+    button1 = create_buttons("answer1", 85, 5)
+    button2 = create_buttons("answer2", 5, 85)
+    button3 = create_buttons("answer3", 85, 5)
+    button4 = create_buttons("answer4", 5, 85)
+
+    widgets["answer1"].append(button1)
+    widgets["answer2"].append(button2)
+    widgets["answer3"].append(button3)
+    widgets["answer4"].append(button4)
+
+    # place widget on the grid
+    grid.addWidget(widgets["question"][-1], 1, 0, 1, 2)
+    grid.addWidget(widgets["answer1"][-1], 2, 0)
+    grid.addWidget(widgets["answer2"][-1], 2, 1)
+    grid.addWidget(widgets["answer3"][-1], 3, 0)
+    grid.addWidget(widgets["answer4"][-1], 3, 1)
+
+def frame_input_text():
+    clear_widgets()
+    # question widget
+    question = QLabel("QUESTION4button")
+    question.setAlignment(QtCore.Qt.AlignCenter)
+    question.setWordWrap(True)
+    question.setStyleSheet(
+        "font-family: Shanti;" +
+        "font-size: 25px;" +
+        "color: 'white';" +
+        "padding: 75px;"
+
+    )
+    widgets["question"].append(question)
+
+    textbox = QLineEdit()
+    textbox.setStyleSheet(
+        '''
+        *{
+            border: 4px solid '#BC006C';
+            border-radius: 45px;
+            font-size: 35px;
+            color: 'white';
+            padding: 25px 0;
+            margin: 100px 200px;
+        }
+        *:hover{
+            background: '#BC006C';
+        }
+        '''
+    )
+    textbox.move(20, 20)
+    textbox.resize(280, 40)
+
+def next_question_frame():
+    # display frame for next question
+    clear_widgets()
+
+    #dict = utils.makeDictionaryBool(model.current_question)
+
+
+
 def create_buttons(answer, l_margin, r_margin):
     '''create identical buttons with custom left & right margins'''
     button = QPushButton(answer)
